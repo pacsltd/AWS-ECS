1dee20fdc5093da2699fc81b5b663d62aab6b83b
philanimalandrews <65134755+philanimalandrews@users.noreply.github.com>
2023-04-28 11:35:38 +0100

    first commit

---
+++ src/test/java/com/example/ecs3/Ecs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecs3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
